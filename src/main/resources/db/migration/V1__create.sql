74b3c6711d0ba5b24d35182c1599a7c9417b49bc
ArtemAlt <artem.altunin.vit@gmail.com>
2021-08-27 14:36:58 +0300

    created table: goods category goods_category contacts producers good_producer

--- src/main/resources/db/migration/V1__create.sql
+++ src/main/resources/db/migration/V1__create.sql
@@ -24,4 +24,60 @@ CREATE TABLE users_roles
     role_id bigint not null references roles (id),
     primary key (user_id, role_id)
 );
+create table goods
+(
+    id          bigserial primary key,
+    vendor_code integer        not null unique,
+    title       varchar(255)   not null,
+    price       numeric(10, 2) not null,
+    url_preview varchar(255),
+    url_full    varchar(255),
+    description varchar,
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
+);
+create table category
+(
+    id            bigserial primary key,
+    category_name varchar(50) not null unique
+);
+
+CREATE TABLE goods_category
+(
+    goods_id    bigint not null references goods (id),
+    category_id bigint not null references category (id),
+    primary key (goods_id, category_id)
+);
+create table contacts
+(
+    id              bigserial primary key,
+    FIO             varchar(255) not null unique,
+    position        varchar(50),
+    primary_phone   integer      not null unique,
+    mobile_phone    integer,
+    work_phone      integer,
+    primary_email   varchar(255) not null,
+    secondary_email varchar(255),
+    created_at      timestamp default current_timestamp,
+    updated_at      timestamp default current_timestamp
+);
+create table producers
+(
+    id          bigserial primary key,
+    id_contact  bigint  not null references contacts (id),
+    designation varchar(255) unique,
+    INN         integer not null unique,
+    description varchar(255),
+    web_site    varchar(255),
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
+);
+create table good_producer
+(
+    goods_id    bigint not null references goods (id),
+    producer_id bigint not null references producers (id),
+    primary key (goods_id, producer_id)
+);
+
+
 
