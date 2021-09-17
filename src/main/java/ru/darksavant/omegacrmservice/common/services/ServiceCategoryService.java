592321a80cbeed2c7144bb25ec181ffed504f620
Артем Алтунин <artem.altunin.vit@gmail.com>
2021-09-17 23:50:38 +0300

    create: ServiceService ServiceCategoryService

---
+++ src/main/java/ru/darksavant/omegacrmservice/common/services/ServiceCategoryService.java
@@ -0,0 +1,32 @@
+package ru.darksavant.omegacrmservice.common.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.darksavant.omegacrmservice.common.entities.ServiceCategory;
+import ru.darksavant.omegacrmservice.common.repositories.ServiceCategoryRepository;
+import ru.darksavant.omegacrmservice.common.repositories.ServiceRepository;
+import ru.darksavant.omegacrmservice.errors.ResourceNotFoundException;
+
+@Service
+@AllArgsConstructor
+public class ServiceCategoryService {
+    private final ServiceCategoryRepository serviceCategoryRepository;
+
+    public ServiceCategory findByDescription(String name) {
+        return serviceCategoryRepository.findByName(name).orElseThrow(() -> new ResourceNotFoundException("ServiceCategory not found"));
+    }
+
+    public ServiceCategory save(ServiceCategory d) {
+        return serviceCategoryRepository.save(d);
+    }
+
+    public void deleteDiscount(ServiceCategory d) {
+        serviceCategoryRepository.delete(d);
+    }
+
+    public void deleteDiscountById(Long id) {
+        serviceCategoryRepository.deleteById(id);
+    }
+
+
+}
