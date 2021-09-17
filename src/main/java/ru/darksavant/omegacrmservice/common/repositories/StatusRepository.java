e498524c29584e7d506e11b89fff9994b5beafde
Артем Алтунин <artem.altunin.vit@gmail.com>
2021-09-17 23:57:09 +0300

    create: GoodsCategoryService StatusService

--- src/main/java/ru/darksavant/omegacrmservice/common/repositories/StatusRepository.java
+++ src/main/java/ru/darksavant/omegacrmservice/common/repositories/StatusRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.darksavant.omegacrmservice.common.entities.Status;
 
+import java.util.Optional;
+
 
 @Repository
 public interface StatusRepository extends JpaRepository<Status,Long> {
 
+    Optional<Status> findByTitle(String name);
 }
---
+++ src/main/java/ru/darksavant/omegacrmservice/common/services/GoodsCategoryService.java
@@ -0,0 +1,33 @@
+package ru.darksavant.omegacrmservice.common.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.darksavant.omegacrmservice.common.entities.Category;
+import ru.darksavant.omegacrmservice.common.entities.Status;
+import ru.darksavant.omegacrmservice.common.repositories.CategoryRepository;
+import ru.darksavant.omegacrmservice.common.repositories.StatusRepository;
+import ru.darksavant.omegacrmservice.errors.ResourceNotFoundException;
+
+@Service
+@AllArgsConstructor
+public class GoodsCategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public Category findByName(String name) {
+        return categoryRepository.findByName(name).orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+    }
+
+    public Category save(Category d) {
+        return categoryRepository.save(d);
+    }
+
+    public void deleteDiscount(Category d) {
+        categoryRepository.delete(d);
+    }
+
+    public void deleteDiscountById(Long id){
+        categoryRepository.deleteById(id);
+    }
+
+
+}
